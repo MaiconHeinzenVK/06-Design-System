4eaa30015d68f0f30e132bf8b0413f2b0a2b4347
MaiconHeinzen <m41conv1k1ng@gmail.com>
2024-02-02 21:32:55 -0300

    feat: :sparkles: Componentes do Design System - Componente: Avatar

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { AvatarContainer, AvatarFallback, AvatarImage } from './style'
+import { User } from 'phosphor-react'
+import { ComponentProps } from 'react'
+
+export type AvatarProps = ComponentProps<typeof AvatarImage>
+
+export function Avatar() {
+  return (
+    <AvatarContainer>
+      <AvatarImage />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
